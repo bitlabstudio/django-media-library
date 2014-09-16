b241431afa783640fa9d013feaca438550269490
Daniel Kaufhold <kaufhold.d@gmail.com>
2014-09-16 12:12:28 +0200

    Added forms

---
+++ media_library/forms.py
@@ -0,0 +1,60 @@
+"""Forms for the ``media_library`` app."""
+from django.utils.translation import get_language, ugettext_lazy as _
+
+from hvad.forms import TranslatableModelForm
+from multilingual_tags.forms.mixins import TaggingFormMixin
+
+from . import models
+
+
+class MediaItemImageForm(TaggingFormMixin, TranslatableModelForm):
+    """Form to edit and create meta data for library images."""
+
+    language = get_language()
+    tag_field = {
+        'name': 'tags',
+        'label': _('Tags'),
+        'help_text': _('You can add up to 3 tags separated by comma.'),
+        'required': False,
+        'max_tags': 3,
+    }
+
+    def __init__(self, user, image, *args, **kwargs):
+        super(MediaItemImageForm, self).__init__(*args, **kwargs)
+        self.image = image
+        self.library = user.medialibraries.all()[0]
+        self.fields['date'].widget.attrs.update({'data-class': 'datepicker'})
+
+    def save(self, commit=True):
+        self.instance.library = self.library
+        self.instance.image = self.image
+        return super(MediaItemImageForm, self).save(commit)
+
+    class Meta:
+        model = models.MediaItem
+        fields = ['date', 'location']
+
+
+class MediaItemVideoForm(TaggingFormMixin, TranslatableModelForm):
+
+    language = get_language()
+    tag_field = {
+        'name': 'tags',
+        'label': _('Tags'),
+        'help_text': _('You can add up to 3 tags separated by comma.'),
+        'required': False,
+        'max_tags': 3,
+    }
+
+    def __init__(self, user, *args, **kwargs):
+        super(MediaItemVideoForm, self).__init__(*args, **kwargs)
+        self.library = user.medialibraries.all()[0]
+        self.fields['date'].widget.attrs.update({'data-class': 'datepicker'})
+
+    def save(self, commit=True):
+        self.instance.library = self.library
+        return super(MediaItemVideoForm, self).save(commit)
+
+    class Meta:
+        model = models.MediaItem
+        fields = ['video', 'date', 'location']
---
+++ media_library/tests/forms_tests.py
@@ -0,0 +1,106 @@
+"""Tests for the forms of the ``media_library`` app."""
+from django.test import TestCase
+
+from django_libs.tests.factories import UserFactory
+from user_media.tests.factories import UserMediaImageFactory
+from multilingual_tags.models import Tag, TaggedItem
+
+from . import factories
+from .. import forms, models
+
+
+class MediaItemImageFormTestCase(TestCase):
+    """Tests for the ``MediaItemImageForm`` form class."""
+    longMessage = True
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.library = factories.MediaLibraryFactory(user=self.user)
+        self.image = UserMediaImageFactory(
+            user=self.user, content_object=self.library)
+        self.data = {
+            'location': u'Berlin',
+            'date': u'12/12/2012',
+            'tags': u'foo, bar, foobar'
+        }
+        self.data_update = {
+            'location': u'Hamburg',
+        }
+
+    def test_form(self):
+        form = forms.MediaItemImageForm(
+            data=self.data, user=self.user, image=self.image,
+        )
+        self.assertTrue(form.is_valid(), msg=(
+            'The form should be valid. Errors: {0}'.format(form.errors)))
+
+        instance = form.save()
+        self.assertEqual(models.MediaItem.objects.count(), 1, msg=(
+            'The form should have created one MediaItem.'
+        ))
+        self.assertEqual(Tag.objects.count(), 3, msg=(
+            'The form should have created 3 tags.'
+        ))
+        self.assertEqual(TaggedItem.objects.count(), 3, msg=(
+            'The form should have created 3 tagged items.'
+        ))
+
+        form = forms.MediaItemImageForm(
+            data=self.data_update, user=self.user, image=self.image,
+            instance=instance,
+            )
+        self.assertTrue(form.is_valid(), msg=(
+            'The form should be valid. Errors: {0}'.format(form.errors)))
+
+        instance = form.save()
+        self.assertEqual(models.MediaItem.objects.count(), 1, msg=(
+            'There should still be only one MediaItem.'
+        ))
+        self.assertEqual(Tag.objects.count(), 3, msg=(
+            'There should still be only 3 tags'
+        ))
+        self.assertEqual(TaggedItem.objects.count(), 3, msg=(
+            'There should still be only 3 tagged items.'
+        ))
+        self.assertEqual(instance.location, 'Hamburg', msg=(
+            'The form should have updated the location field.'
+        ))
+
+
+class MediaItemVideoFormTestCase(TestCase):
+    """Tests for the ``MediaItemVideoForm`` form class."""
+    longMessage = True
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.library = factories.MediaLibraryFactory(user=self.user)
+        self.data = {
+            'video': u'https://youtube.com/watch?v=123456',
+            'location': u'Berlin',
+            'date': u'12/12/2012',
+            'tags': u'foo, bar, foobar'
+        }
+        self.data_update = {
+            'location': u'Hamburg',
+            }
+
+    def test_form(self):
+        form = forms.MediaItemVideoForm(data=self.data, user=self.user)
+        self.assertTrue(form.is_valid(), msg=(
+            'The form should be valid. Errors: {0}'.format(form.errors)))
+
+        instance = form.save()
+        self.assertEqual(models.MediaItem.objects.count(), 1, msg=(
+            'The form should have created one MediaItem.'
+        ))
+
+        form = forms.MediaItemVideoForm(
+            data=self.data_update, user=self.user, instance=instance,
+            )
+        self.assertTrue(form.is_valid(), msg=(
+            'The form should be valid. Errors: {0}'.format(form.errors)))
+
+        form.save()
+        self.assertEqual(models.MediaItem.objects.count(), 1, msg=(
+            'There should still be only one MediaItem.'
+        ))
