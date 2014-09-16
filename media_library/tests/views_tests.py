dffca9634cb0449e1acc0388523c387f1c3912ff
Daniel Kaufhold <kaufhold.d@gmail.com>
2014-09-16 12:27:35 +0200

    Added views

---
+++ media_library/tests/views_tests.py
@@ -0,0 +1,155 @@
+"""Tests for the views of the ``media_library`` app."""
+from django.test import TestCase
+from django.core.urlresolvers import reverse
+
+from django_libs.tests.factories import UserFactory
+from django_libs.tests.mixins import ViewRequestFactoryTestMixin
+from user_media.tests.factories import UserMediaImageFactory
+
+from .. import models
+from . import factories
+from .. import views
+
+
+# Test Mixins =================================================================
+
+class MediaItemImageCRUDViewTestMixin(ViewRequestFactoryTestMixin):
+
+    def get_view_kwargs(self):
+        return {'image_pk': self.image.pk}
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.other_user = UserFactory()
+        self.library = factories.MediaLibraryFactory(user=self.user)
+        self.image = UserMediaImageFactory(
+            user=self.user, content_object=self.library,
+        )
+        self.data = {
+            'location': u'Berlin',
+        }
+
+    def test_view(self):
+        self.should_redirect_to_login_when_anonymous()
+        self.is_callable(user=self.user)
+        self.is_postable(
+            user=self.user, data=self.data, to=reverse('medialibrary_edit'),
+        )
+
+        if hasattr(self, 'mediaitem'):
+            self.is_not_callable(user=self.other_user)
+
+
+class MediaItemVideoCRUDViewTestMixin(ViewRequestFactoryTestMixin):
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.other_user = UserFactory()
+        self.library = factories.MediaLibraryFactory(user=self.user)
+        self.data = {
+            'location': u'Berlin',
+            'video': u'https://youtube.com/watch?v=123456',
+        }
+
+    def test_view(self):
+        self.should_redirect_to_login_when_anonymous()
+        self.is_callable(user=self.user)
+        self.is_postable(
+            user=self.user, data=self.data, to=reverse('medialibrary_edit'),
+        )
+        if hasattr(self, 'mediaitem'):
+            self.is_not_callable(user=self.other_user)
+
+
+# Tests =======================================================================
+
+class MediaLibraryEditViewTestCase(ViewRequestFactoryTestMixin, TestCase):
+    """Tests for the ``MediaLibraryEditView`` view class."""
+    view_class = views.MediaLibraryEditView
+
+    def setUp(self):
+        self.user = UserFactory()
+
+    def test_view(self):
+        self.should_redirect_to_login_when_anonymous()
+        self.is_callable(user=self.user)
+
+
+class MediaItemDeleteViewTestCase(ViewRequestFactoryTestMixin, TestCase):
+    """Tests for the ``MediaItemDeleteView`` view class."""
+    view_class = views.MediaItemDeleteView
+
+    def get_view_kwargs(self):
+        return {'pk': self.mediaitem.pk}
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.other_user = UserFactory()
+        self.mediaitem = factories.MediaItemFactory(
+            image=UserMediaImageFactory(),
+            library__user=self.user)
+
+    def test_view(self):
+        self.should_redirect_to_login_when_anonymous()
+        self.is_callable(user=self.user)
+        self.is_not_callable(user=self.other_user)
+        self.is_postable(user=self.user, data={}, to=reverse(
+            'medialibrary_edit'))
+        self.assertEqual(models.MediaItem.objects.count(), 0, msg=(
+            'The media item should be deleted.'
+        ))
+
+
+class MediaItemImageCreateViewTestCase(MediaItemImageCRUDViewTestMixin,
+                                          TestCase):
+    """Tests for the ``MediaItemImageCreateView`` view class."""
+    view_class = views.MediaItemImageCreateView
+
+
+class MediaItemImageUpdateViewTestCase(MediaItemImageCRUDViewTestMixin,
+                                          TestCase):
+    """Tests for the ``MediaItemImageUpdateView`` view class."""
+    view_class = views.MediaItemImageUpdateView
+
+    def setUp(self):
+        super(MediaItemImageUpdateViewTestCase, self).setUp()
+        self.mediaitem = factories.MediaItemFactory(
+            image=self.image, library=self.library,
+        )
+
+
+class MediaItemVideoCreateViewTestCase(MediaItemVideoCRUDViewTestMixin,
+                                          TestCase):
+    """Tests for the ``MediaItemVideoCreateView`` view class."""
+    view_class = views.MediaItemVideoCreateView
+
+
+class MediaItemVideoUpdateViewTestCase(MediaItemVideoCRUDViewTestMixin,
+                                          TestCase):
+    """Tests for the ``MediaItemVideoUpdateView`` view class."""
+    view_class = views.MediaItemVideoUpdateView
+
+    def get_view_kwargs(self):
+        return {'pk': self.mediaitem.pk}
+
+    def setUp(self):
+        super(MediaItemVideoUpdateViewTestCase, self).setUp()
+        self.mediaitem = factories.MediaItemFactory(
+            library=self.library, user=self.user,
+        )
+
+
+class MediaLibraryUploadViewTestCase(ViewRequestFactoryTestMixin, TestCase):
+    """Tests for the ``MediaLibraryUploadView`` view class."""
+    view_class = views.MediaLibraryUploadView
+
+    def setUp(self):
+        self.user = UserFactory()
+        self.library = factories.MediaLibraryFactory(user=self.user)
+
+    def test_view(self):
+        self.should_redirect_to_login_when_anonymous()
+        self.is_callable(user=self.user)
+        self.assertEqual(models.MediaLibrary.objects.count(), 1, msg=(
+            'There should be one media item automatically created.'
+        ))
--- media_library/views.py
+++ media_library/views.py
@@ -1,8 +1,160 @@
-"""Views for the media_library app."""
-# from django.views.generic import TemplateView
+"""Views for the ``media_library`` app."""
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse
+from django.http import Http404
+from django.utils.decorators import method_decorator
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    TemplateView,
+    UpdateView,
+)
+from django.shortcuts import get_object_or_404, redirect
 
-# from . import models
+from django_libs.views_mixins import AjaxResponseMixin
+from multilingual_tags.models import Tag
+from user_media.models import UserMediaImage
+
+from . import models
+from . import forms
 
 
-# class YourView(TemplateView):
-#    template_name = 'media_library/default.html'
+# Mixins ======================================================================
+
+class MediaItemImageCRUDViewMixin(object):
+    form_class = forms.MediaItemImageForm
+    model = models.MediaItem
+    template_name = 'media_library/mediaitem_image_form.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        self.image = get_object_or_404(
+            UserMediaImage, pk=kwargs.get('image_pk'),
+        )
+        if self.image.user != request.user:
+            raise Http404
+        return super(MediaItemImageCRUDViewMixin, self).dispatch(
+            request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(MediaItemImageCRUDViewMixin, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user, 'image': self.image})
+        return kwargs
+
+    def get_context_data(self, **kwargs):
+        ctx = super(MediaItemImageCRUDViewMixin, self).get_context_data(
+            **kwargs)
+        tags = [str(t.name) for t in Tag.objects.get_for_model(self.model)]
+        ctx.update({'image': self.image, 'tags': tags})
+        return ctx
+
+    def get_success_url(self):
+        return reverse('medialibrary_edit')
+
+
+class MediaItemVideoCRUDViewMixin(CreateView):
+    form_class = forms.MediaItemVideoForm
+    model = models.MediaItem
+    template_name = 'media_library/mediaitem_video_form.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        if 'pk' in kwargs:
+            self.object = self.get_object(self.queryset)
+            if self.object.library.user != request.user:
+                raise Http404
+        return super(MediaItemVideoCRUDViewMixin, self).dispatch(
+            request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(MediaItemVideoCRUDViewMixin, self).get_context_data(
+            **kwargs)
+        tags = [str(t.name) for t in Tag.objects.get_for_model(self.model)]
+        ctx.update({'tags': tags})
+        return ctx
+
+    def get_form_kwargs(self):
+        kwargs = super(MediaItemVideoCRUDViewMixin, self).get_form_kwargs()
+        kwargs.update({'user': self.request.user})
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('medialibrary_edit')
+
+
+# Views =======================================================================
+
+class MediaLibraryEditView(AjaxResponseMixin, DetailView):
+    """A view to edit media details of a user on its profile."""
+    template_name = 'media_library/medialibrary_edit.html'
+    model = models.MediaLibrary
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(MediaLibraryEditView, self).dispatch(
+            request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(MediaLibraryEditView, self).get_context_data(**kwargs)
+        new_images = self.object.user_media_images.filter(
+            media_items__isnull=True,
+        )
+        ctx.update({'new_images': new_images})
+        return ctx
+
+    def get_object(self, queryset=None):
+        library, created = models.MediaLibrary.objects.get_or_create(
+            user=self.request.user
+        )
+        return library
+
+
+class MediaItemDeleteView(DeleteView):
+    model = models.MediaItem
+    template_name = 'media_library/mediaitem_delete.html'
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        self.object = self.get_object(self.queryset)
+        if self.object.library.user != request.user:
+            raise Http404
+        return super(MediaItemDeleteView, self).dispatch(request, *args,
+                                                            **kwargs)
+
+    def get_success_url(self):
+        return reverse('medialibrary_edit')
+
+
+class MediaItemImageCreateView(MediaItemImageCRUDViewMixin, CreateView):
+    """View to create meta data for a media item."""
+
+
+class MediaItemImageUpdateView(MediaItemImageCRUDViewMixin, UpdateView):
+    """View to update meta data for a media item."""
+
+    def get_object(self, queryset=None):
+        return self.image.media_items.all()[0]
+
+
+class MediaItemVideoCreateView(MediaItemVideoCRUDViewMixin, CreateView):
+    """View to create meta data for a media item."""
+
+
+class MediaItemVideoUpdateView(MediaItemVideoCRUDViewMixin, UpdateView):
+    """View to update meta data for a media item."""
+
+
+class MediaLibraryUploadView(TemplateView):
+    """View where users upload their pictures."""
+    template_name = "media_library/medialibrary_upload.html"
+
+    @method_decorator(login_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(MediaLibraryUploadView, self).dispatch(request, *args,
+                                                        **kwargs)
+
+    def get_context_data(self, **kwargs):
+        ctx = super(MediaLibraryUploadView, self).get_context_data(**kwargs)
+        ctx.update({'library': self.request.user.medialibraries.all()[0]})
+        return ctx
