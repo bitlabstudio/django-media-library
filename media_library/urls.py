6db6e861377ee3fb83cfe99b0e6c3cbb43f79023
Daniel Kaufhold <kaufhold.d@gmail.com>
2014-09-16 12:27:58 +0200

    Added urls

--- media_library/urls.py
+++ media_library/urls.py
@@ -1,12 +1,28 @@
-"""URLs for the media_library app."""
-# from django.conf.urls import patterns, url
+"""URLs for the ``media_library`` app."""
+from django.conf.urls import patterns, url
 
-# from . import views
+from . import views
 
 
-# urlpatterns = patterns(
-#     '',
-#     url(r'^$',
-#         views.YourView.as_view(),
-#         name='media_library_default'),
-# )
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.MediaLibraryEditView.as_view(),
+        name='medialibrary_edit'),
+    url(r'^upload/$', views.MediaLibraryUploadView.as_view(),
+        name='medialibrary_upload'),
+    url(r'^image/create/(?P<image_pk>\d+)/$',
+        views.MediaItemImageCreateView.as_view(),
+        name='mediaitem_image_create'),
+    url(r'^image/update/(?P<image_pk>\d+)/$',
+        views.MediaItemImageUpdateView.as_view(),
+        name='mediaitem_image_update'),
+    url(r'^video/create/$',
+        views.MediaItemVideoCreateView.as_view(),
+        name='mediaitem_video_create'),
+    url(r'^video/update/(?P<pk>\d+)/$',
+        views.MediaItemVideoUpdateView.as_view(),
+        name='mediaitem_video_update'),
+    url(r'^delete/(?P<pk>\d+)/$',
+        views.MediaItemDeleteView.as_view(),
+        name='mediaitem_delete'),
+)
